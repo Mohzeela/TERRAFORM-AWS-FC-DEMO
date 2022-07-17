f28bd9a506c5b8b2ff410bd4c0404c20f3a436ea
walemoh <olawale.mohammed@deimos.co.za>
2022-07-17 17:17:37 +0100

    updated tf.yml

--- main.tf
+++ main.tf
@@ -1,32 +1,20 @@
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-#   name = "my-vpc"
-#   cidr = "10.0.0.0/16"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-#   azs             = ["eu-east-1a", "eu-east-1b", "eu-east-1c"]
-#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.availability_zones
+  private_subnets = var.private_subnets_cidr
+  public_subnets  = var.public_subnets_cidr
 
-#   enable_nat_gateway = true
-#   enable_vpn_gateway = true
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
-#   tags = {
-#     Terraform = "true"
-#     Environment = "production"
-#   }
-# }
-
-module "network" {
-  source               = "./modules/network"
-  region               = var.region
-  environment          = var.environment
-  vpc_cidr             = var.vpc_cidr
-  public_subnets_cidr  = var.public_subnets_cidr
-  private_subnets_cidr = var.private_subnets_cidr
-  availability_zones   = var.availability_zones
-
-
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 # module "instance" {
